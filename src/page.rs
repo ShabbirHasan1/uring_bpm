621d607d539fa8e15bc4003292c9b39b42dd1d16
Connor Tsui <connor.tsui20@gmail.com>
2024-03-17 21:07:53 -0400

    refactor again

--- src/page.rs
+++ src/page.rs
@@ -1,10 +1,15 @@
 use crate::{buffer_pool::BufferPool, frame::Frame};
 use std::io;
+use std::sync::atomic::{AtomicU8, Ordering};
 use std::{
     ops::{Deref, DerefMut},
     sync::Arc,
 };
 use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+
+const HOT: u8 = 0;
+const COOL: u8 = 1;
+const COLD: u8 = 2;
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct PageId {
@@ -20,69 +25,87 @@ impl From<PageId> for u64 {
 
 pub const PAGE_SIZE: usize = 1 << 12; // 4096
 
+pub struct Page {
+    /// The unique ID of the logical page of data.
+    pid: PageId,
+
+    /// 3 States: Hot, Cool, and Cold
+    /// If it is Hot or Cool, then the page is in memory (Some variant)
+    /// If it is Cold, the page is not in memory (None variant)
+    state: AtomicU8,
+
+    /// Protected Swip
+    swip: RwLock<Swip>,
+
+    /// Pointer back to the Buffer Pool
+    bpm: Arc<BufferPool>,
+}
+
 #[derive(Default)]
 pub struct Swip {
     data: Option<Frame>,
 }
 
-#[derive(Clone)]
-pub struct Page {
-    /// The unique ID of the logical page of data.
-    pid: PageId,
-    /// Pointer back to the Buffer Pool
-    bpm: Arc<BufferPool>,
-    /// A [`RwLock`]-protected Swip
-    swip: Arc<RwLock<Swip>>,
-}
-
 impl Page {
-    fn new(pid: PageId, bpm: Arc<BufferPool>) -> Self {
-        Self {
-            pid,
-            bpm,
-            swip: Arc::new(RwLock::new(Swip::default())),
-        }
-    }
-
-    pub fn pid(&self) -> PageId {
-        self.pid
+    /// Changes state from `COOL` to `HOT. Does not change state if it is `COLD` or `HOT`.
+    ///
+    /// We don't care if this fails, since if it was already `HOT` then we don't need to to
+    /// anything, and if it was `COLD`, we don't want to change it.
+    fn make_hot(&self) {
+        // Ignore the result
+        let _ = self
+            .state
+            .compare_exchange(COOL, HOT, Ordering::Release, Ordering::Relaxed);
     }
 
     /// Reads a page.
     pub async fn read(&self) -> ReadPageGuard {
-        let guard = self.swip.read().await;
-        if guard.data.is_some() {
-            // We checked that the data is the `Some` variant, so this will not panic
-            return ReadPageGuard::new(guard).unwrap();
-        }
-        drop(guard);
+        self.make_hot();
 
-        // The page is not in memory, so we need to bring it in
-        let mut write_guard = self.swip.write().await;
-        if write_guard.data.is_some() {
-            // TODO is this even possible?
-            // Someone other writer got in front of us and updated for us
+        {
+            let guard = self.swip.read().await;
+            if guard.data.is_some() {
+                // We checked that the data is the `Some` variant, so this will not panic
+                return ReadPageGuard::new(guard).unwrap();
+            }
+        }
+
+        {
+            // The page is not in memory, so we need to bring it in
+            let mut write_guard = self.swip.write().await;
+
+            if write_guard.data.is_some() {
+                // Someone other writer got in front of us and updated for us
+                assert_ne!(self.state.load(Ordering::Acquire), COLD);
+                self.make_hot();
+
+                return ReadPageGuard::new(write_guard.downgrade()).unwrap();
+            }
+
+            assert_eq!(self.state.load(Ordering::Acquire), COLD);
+
+            // We need to go grab a free frame
+            let frame = self.bpm.get_free_frame().await;
+
+            // Bring in new data to that frame
+            let (res, frame) = self.bpm.disk_manager().read(self.pid, frame).await;
+            res.expect("Unable to read in data from disk");
+
+            // Give ownership to the `Swip`
+            write_guard.data.replace(frame);
+
+            // Since we only just brought this into memory, it is a hot page
+            self.state.store(HOT, Ordering::Release);
+
+            // Return the downgraded guard that now has the valid data
             return ReadPageGuard::new(write_guard.downgrade()).unwrap();
         }
-
-        // We need to go grab a free frame
-        let frame = self.bpm.get_free_frame().await;
-
-        // Bring in new data to that frame
-        let (res, frame) = self.bpm.disk_manager().read(self.pid, frame).await;
-        res.expect("Unable to read in data from disk");
-
-        // Give ownership to the Swip
-        write_guard.data.replace(frame);
-
-        // Return the downgraded guard that now has the valid data
-        return ReadPageGuard::new(write_guard.downgrade()).unwrap();
     }
 
     /// Writes to a page.
-    ///
-    /// Can use this function for eviction.
     pub async fn write(&self) -> WritePageGuard {
+        self.make_hot();
+
         let mut guard = self.swip.write().await;
         if guard.data.is_some() {
             // We checked that the data is the `Some` variant, so this will not panic
@@ -96,8 +119,11 @@ impl Page {
         let (res, frame) = self.bpm.disk_manager().read(self.pid, frame).await;
         res.expect("Unable to read in data from disk");
 
-        // Give ownership to the `Swip``
+        // Give ownership to the `Swip`
         guard.data.replace(frame);
+
+        // Since we only just brought this into memory, it is a hot page
+        self.state.store(HOT, Ordering::Release);
 
         // Return the guard that now has the valid data
         return WritePageGuard::new(guard).unwrap();
@@ -105,7 +131,10 @@ impl Page {
 
     /// Evicts a page and returns back the frame it used to own
     pub async fn evict(&self) -> io::Result<Frame> {
+        self.state.store(COLD, Ordering::Release);
+
         let mut guard = self.swip.write().await;
+
         if guard.data.is_none() {
             return Err(io::Error::new(
                 io::ErrorKind::NotFound,
@@ -178,6 +207,7 @@ impl<'a> WritePageGuard<'a> {
             eprintln!(
                 "Trying to construct a WritePageGuard with a swip that does not have any data"
             );
+
             return None;
         }
 
